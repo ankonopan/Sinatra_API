f03c823cc1be79a9bdfe1dbed530305ed68deddc
Jose Antonio Pio Gil <josetonyp@latizana.com>
2015-04-16 13:52:30 +0200

    Adding Error messages to UI

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -17,12 +17,31 @@ var Consumer = angular.module('Consumer', [
 .controller('HomeController', ['$scope', 'Restangular', function($scope, Restangular) {
   var offersApi = Restangular.oneUrl("offers");
   $scope.offers = [];
-  $scope.offerQuery = {};
+  $scope.errorFields = false;
+  $scope.offerQuery = { page: "1" };
+  $scope.emptyOffers = false;
+  $scope.processingOffers = false;
 
   $scope.searchOffers = function(event) {
-    offersApi.post("" , $scope.offerQuery ).then(function( offers ) {
-      $scope.offers = offers.offers;
-    });
+    if( _.isString( $scope.offerQuery.uid ) &&  $scope.offerQuery.uid != "" &&
+        _.isString( $scope.offerQuery.pub0 ) && $scope.offerQuery.pub0 != "" &&
+        _.isString( $scope.offerQuery.page )  && $scope.offerQuery.page != ""
+      ){
+
+        $scope.processingOffers = true;
+        $scope.errorFields = false;
+        offersApi.post("" , $scope.offerQuery ).then(function( offers ) {
+          if ( _.isUndefined(offers.offers) || offers.offers.length == 0) {
+            $scope.emptyOffers = true;
+          }else{
+            $scope.emptyOffers = false;
+            $scope.offers = offers.offers;
+          };
+          $scope.processingOffers = false;
+        });
+    }else{
+      $scope.errorFields = true;
+    }
     return false;
   };
 
